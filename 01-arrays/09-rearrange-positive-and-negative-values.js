adf19a13683e3c3f2e15b158f818081d6bae39b3
Alvee Akand <AlveeM@users.noreply.github.com>
2020-02-14 18:40:12 -0500

    added rearrange-positive-and-negative-values in arrays

---
+++ 01-arrays/09-rearrange-positive-and-negative-values.js
@@ -0,0 +1,19 @@
+const rearrange = arr => {
+    let leftMostPosIdx = 0;
+    let temp = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            if (i !== leftMostPosIdx) {
+                temp = arr[i];
+                arr[i] = arr[leftMostPosIdx];
+                arr[leftMostPosIdx] = temp;
+            }
+            leftMostPosIdx++;
+        }
+    }
+
+    return arr;
+}
+
+console.log(rearrange([10, -1, 20, 4, 5, -9, -6]));
