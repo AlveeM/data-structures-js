c7fa88e5420e311008f0d9740fc3a00e47110566
Alvee Akand <AlveeM@users.noreply.github.com>
2020-02-18 18:42:40 -0500

    added sort-values-in-stack in stack-queue

---
+++ 03-stack-queue/05-sort-values-in-stack.js
@@ -0,0 +1,20 @@
+const Stack = require('./stack');
+
+const sortStack = stack => {
+    if (!stack.isEmpty()) {
+        let value = stack.pop();
+        sortStack(stack);
+        insert(stack, value);
+    }
+    return stack;
+}
+
+const insert = (stack, value) => {
+    if (stack.isEmpty() || value < stack.getTop()) {
+        stack.push(value);
+    } else {
+        let temp = stack.pop();
+        insert(stack, value);
+        stack.push(temp);
+    }
+}
