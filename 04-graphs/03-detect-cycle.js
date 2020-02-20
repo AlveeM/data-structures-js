3dd16724844830a87525e446c4f844205c6f0442
Alvee Akand <AlveeM@users.noreply.github.com>
2020-02-20 10:32:07 -0500

    added detect-cycle in graphs

---
+++ 04-graphs/03-detect-cycle.js
@@ -0,0 +1,33 @@
+const Stack = require('../03-stack-queue/stack');
+const Graph = require('../04-graphs/Graph');
+
+const detectCycle = (g, source) => {
+    let numVertices = g.vertices;
+    let visited = Array(numVertices).fill(false);
+    let stack = new Stack(numVertices);
+    stack.push(source);
+    visited[source] = true;
+    while (!stack.isEmpty()) {
+        let currentNode = stack.pop();
+        let temp = g.list[currentNode].getHead();
+        while (temp !== null) {
+            if (visited[temp.data] === false) {
+                stack.push(temp.data);
+                visited[temp.data] = true;
+            } else {
+                return true;
+            }
+            temp = temp.next;
+        }
+    }
+    return false;
+}
+
+let g=new Graph(5);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(1, 4);
+console.log(detectCycle(g, 0));
+g.addEdge(4, 0)
+console.log(detectCycle(g, 0));
